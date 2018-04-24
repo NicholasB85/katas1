b33ad978502ca09e94fcc7c456d9e63e831d350e
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-04-24 14:23:34 -0400

    assessment

---
+++ javascript/assessment.js
@@ -0,0 +1,99 @@
+
+// first kata
+function one (){
+for (i = 1; i <= 20; i++ ) {
+   console.log(i)
+}
+}
+
+// second kata
+function two (){
+    for (i = 1; i <= 20; i++ ) {
+        if ( i % 2 === 0)
+        console.log(i)
+    }
+}
+
+//third kata
+function three () {
+    var i = 1;
+for(i=1; i<=20; i++) {
+    if(i%2 != 0){
+    console.log(i)
+}
+}
+}
+
+// fouth kata
+function four (){
+    for (i = 1; i <= 100; i++){
+        if (i % 5 == 0)
+        console.log(i)
+    }
+}
+
+// fifth kata
+function five(){
+    var i;
+for (i = 1; i <= 10; i++) {
+    var y = i*i;
+    if (i <= 101){
+
+    console.log(y)
+}
+}
+}
+
+// sixth kata
+function six(){
+    for (i = 20; i > 0; i--) {
+        console.log(i)
+    }
+}
+
+// seven kata
+function seven(){
+    for (i = 20; i > 0; i--){
+        if ( i % 2 == 0){
+        console.log(i)
+    }
+    }
+}
+
+// eighth kata
+function eight() {
+    for (i = 20; i > 0; i--){
+        if ( i % 2 != 0){
+        console.log(i)
+    }
+    }
+}
+
+// ninth kata
+function nine() {
+    for (i = 100; i > 0; i--){
+        if (i % 5 == 0){
+        console.log(i)
+    }
+    }
+}
+
+// tenth kata
+function ten() {
+    for (i = 10; i > 0; i-- ) {
+        var y = i*i;
+        if (i >= 0) {
+            console.log(y)
+        }
+    }
+}
+one()
+two()
+three()
+four()
+five()
+six()
+seven()
+eight()
+nine()
+ten()
